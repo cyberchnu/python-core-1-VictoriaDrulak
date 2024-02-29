2bb8aaca989439de031797bc4c2e057370aa0b77
Victoria <drulakviktoria18@gmail.com>
2024-02-29 18:47:05 +0200

    Task 10 done

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,12 @@
 def mean(number):
   # Type your code
-  return 
+  sum = 0
+  count = 0
+  number_str = str(number)
+  for i in number_str:
+    n = int(i)
+      sum += n
+      count += 1
+      mean = sum / count
+  return mean(number)
+
